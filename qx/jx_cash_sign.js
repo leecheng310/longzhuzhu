cbb4d9dc74fbb8fba4da1d37f82fe00098d69c03
nianyuguai <840311035@qq.com>
2021-04-03 09:02:10 +0800

    add jx_clash_sign

--- qx/jx_cash_sign.js
+++ qx/jx_cash_sign.js
@@ -1,18 +1,18 @@
 /*
-京喜现金签到
+京喜签到消消乐
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 ============Quantumultx===============
 [task_local]
-#京喜现金签到
-10 9 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jx_sign.js, tag=京喜现金签到, enabled=true
+#京喜签到消消乐
+10 9 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jx_sign.js, tag=京喜签到消消乐, enabled=true
 
 ================Loon==============
 [Script]
-cron "5 0 * * *" script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jx_sign.js,tag=京喜现金签到
+cron "5 0 * * *" script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jx_sign.js,tag=京喜签到消消乐
 
 ===============Surge=================
-京喜现金签到 = type=cron,cronexp="5 0 * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jx_sign.js
+ 京喜签到消消乐 = type=cron,cronexp="5 0 * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jx_sign.js
 
 ============小火箭=========
 京喜现金签到 = type=cron,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jx_sign.js, cronexpr="5 0 * * *",timeout=200, enable=true
