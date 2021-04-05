9df02100f0d8a95353122233d2a4e1a275eb24ac
nianyuguai <840311035@qq.com>
2021-04-05 21:14:43 +0800

    add 全局抽奖

--- qx/jd_live_lottery_social.js
+++ qx/jd_live_lottery_social.js
@@ -1,21 +1,21 @@
 /*
- 直播间抽奖（全局）
- 已支持IOS双京东账号,Node.js支持N个京东账号
- 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+直播间抽奖（全局）
+已支持IOS双京东账号,Node.js支持N个京东账号
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 
- [task_local]
- #直播间抽奖（全局）
- 5 8-23/1 * * * https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_live_lottery_social.js, tag=直播间抽奖（全局）, enabled=true
+[task_local]
+#直播间抽奖（全局）
+5 8-23/1 * * * https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_live_lottery_social.js, tag=直播间抽奖（全局）, enabled=true
 
- ================Loon==============
- [Script]
- cron "5 8-23/1 * * *" script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_live_lottery_social.js,tag=直播间抽奖（全局）
+================Loon==============
+[Script]
+cron "5 8-23/1 * * *" script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_live_lottery_social.js,tag=直播间抽奖（全局）
 
- ===============Surge=================
- 直播间抽奖（全局） = type=cron,cronexp="5 8-23/1 * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_live_lottery_social.js
+===============Surge=================
+直播间抽奖（全局） = type=cron,cronexp="5 8-23/1 * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_live_lottery_social.js
 
- ============小火箭=========
- 直播间抽奖（全局）= type=cron,script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_live_lottery_social.js, cronexpr="5 8-23/1 * * *",timeout=200, enable=true
+============小火箭=========
+直播间抽奖（全局）= type=cron,script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_live_lottery_social.js, cronexpr="5 8-23/1 * * *",timeout=200, enable=true
  */
 
 const $ = new Env('直播间抽奖（全局）');
