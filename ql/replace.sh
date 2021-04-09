2024d7853b094dccc8bf8d8c35a996a94e36c992
nianyuguai <840311035@qq.com>
2021-04-09 19:35:17 +0800

    add replace

--- ql/replace.sh
+++ ql/replace.sh
@@ -7,9 +7,12 @@
 # mkdir -p /ql/replace
 # 2、上传三个文件到主机目录/ql/replace
 # 3、修改脚本权限
+# cd /ql/replace
 # chmod +x replace.sh
 # 4、执行替换并重启面板
 # ./replace.sh qinglong
+#
+# 后续维护: 每次容器启动后, 执行最后一个命令替换
 #
 
 env=$1
