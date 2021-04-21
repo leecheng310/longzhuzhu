f3e57bebdcf60dc26d9401de0d8900e4495eefc2
nianyuguai <840311035@qq.com>
2021-04-21 20:22:38 +0800

    update ql

--- ql/replace.sh
+++ ql/replace.sh
@@ -9,7 +9,8 @@ fi
 docker cp api/cookie.ts ${env}:/ql/back/api
 docker cp services/cookie.ts ${env}:/ql/back/services
 docker cp pages/index.tsx ${env}:/ql/src/pages/cookie
-echo "替换文件成功, 重启面板ing"
+echo "替换文件成功, 重启面板ing..."
 docker exec -it ${env} rebuild
 echo "重启面板OK"
+
 
