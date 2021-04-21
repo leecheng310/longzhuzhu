1a86e026367866337a3a0ec0fde389f7c9a30219
nianyuguai <840311035@qq.com>
2021-04-21 23:18:52 +0800

    update ql

--- ql/replace.sh
+++ ql/replace.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-env=$1
-if [ ! $1 ]; then
-  echo "请输入输入容器名词, 例如: qinglong"
-  exit
-fi
+# 【使用方法】:
+# 更新镜像后, 放到自定义脚本当中, 然后更新面板生效
+# 建议: 生效后建议注释脚本, 若乙方爸爸更新幅度比较大可能无法启动
+#
 
-docker cp api/cookie.ts ${env}:/ql/back/api
-docker cp services/cookie.ts ${env}:/ql/back/services
-docker cp pages/index.tsx ${env}:/ql/src/pages/cookie
-echo "替换文件成功, 重启面板ing..."
-docker exec -it ${env} rebuild
-echo "重启面板OK"
+echo '开始下载龙猪猪仓库'
+LONG_DIR=/tmp/longzhuzhu
+rm -rf ${LONG_DIR} && mkdir -p ${LONG_DIR}
+git clone -b main https://github.com/nianyuguai/longzhuzhu.git ${LONG_DIR}
+echo '下载完成, 执行文件替换'
+cp ${LONG_DIR}/ql/api/cookie.ts /ql/back/api
+cp ${LONG_DIR}/ql/services/cookie.ts /ql/back/services
+cp ${LONG_DIR}/ql/pages/index.tsx /ql/src/pages/cookie
+echo '替换完成, 重启面板'
+rebuild >> ${QL_DIR}/log/rebuild.log 2>&1
+echo '重启面板OK'
 
 
